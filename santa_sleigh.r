2b1b0d2c8bf9014134d03743b28271e0a7742c72
harjotsinghparmar <harjot.s.parmar@gmail.com>
2014-01-25 14:41:07 +0530

    sleigh visualisation
    just to help visualize your placement of the presents and help optimize the model

---
+++ santa_sleigh.r
@@ -0,0 +1,8 @@
+wd="d:/" 					#which ever directory you want as working directory
+setwd(wd)					# set workind directory
+file=read.csv("submission.csv")	#read the generated submission file into the a variablefactor(file$z1)
+factor(file$z1)				#factorise according to a particular level
+max(file$PresentId)			#you can check how many presents have been placed as of yet
+slice=file[which(file$z1==(1074625)),]	#slice according to a particular level
+plot(c(0,1000),c(0,1000))
+rect(slice$x1,slice$y1,slice$x4,slice$y4,col="red")
